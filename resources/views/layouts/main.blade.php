919a145f8616a54de29e518c9eac32d43d6b0c93
Serhii Tsapko <info@webizon.com.ua>
2025-11-21 21:56:07 +0100

    refactor meta scripts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang="de">
+<head>
     @include('partials.head')
+</head>
 <body>
     @include('partials.header')
     @yield('content')
     @include('partials.footer')
+    @stack('scripts')
+    <script
+            src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+            crossorigin="anonymous"></script>
+    @vite('resources/js/public/app.js')
 </body>
 </html>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,26 +1,23 @@
-<head>
-    <meta charset="UTF-8">
-    <title>@yield('title')</title>
-    <meta name="description" content="@yield('description')">
-    @hasSection('keywords')
-        <meta name="keywords" content="@yield('keywords')">
-    @endif
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Quicksand:wght@300..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    @vite([
-        'resources/css/public/app.css',
-        'resources/css/public/app.mediascreen.css',
-        'resources/js/public/app.js'
-    ])
-    @stack('head')
-    <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
-    <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
-    <meta name="apple-mobile-web-app-title" content="Reflect-ON" />
-    <link rel="manifest" href="/images/favicon/site.webmanifest" />
-</head>
+<meta charset="UTF-8">
+<meta http-equiv="Content-Language" content="de">
+<title>@yield('title')</title>
+<meta name="description" content="@yield('description')">
+@hasSection('keywords')
+    <meta name="keywords" content="@yield('keywords')">
+@endif
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Quicksand:wght@300..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet">
+@vite([
+    'resources/css/public/app.css',
+    'resources/css/public/app.mediascreen.css'
+])
+@stack('head')
+<link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+<link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Reflect-ON" />
+<link rel="manifest" href="/images/favicon/site.webmanifest" />
