18df9657afd634daabdf9a81c0b3a17eb51ccdef
Serhii Tsapko <info@webizon.com.ua>
2025-11-21 11:58:48 +0100

    temp redirect after login -> /admin/pages

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,13 +27,14 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        //return redirect()->intended(route('dashboard', absolute: false));
+        return Inertia::location('/admin/pages');
     }
 
     /**
